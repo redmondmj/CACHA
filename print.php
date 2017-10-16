de264e7903abc5c1ffce8efa329f33d6a1e2eeb9
Matt Redmond <matt.redmond@nscc.ca>
2017-10-16 04:15:39 -0300

    still testing

--- print.php
+++ print.php
@@ -242,6 +242,25 @@
     }
 
     function getPractitioner($id){
+      $mysql_user = "cacha_web";
+      $mysql_pass = "p2DaavTC03Vr";
+      $mysql_host = "localhost:3306";
+      $db_name = "cacha_medrec";
+  
+      // build the response object
+      class ResponseObject {
+          public $success = false;
+          public $reason = "";
+          public $entries = array();
+      }
+  
+      $response = new ResponseObject();
+      @$connect = new mysqli($mysql_host, $mysql_user, $mysql_pass, $db_name);
+      if ($connect->connect_error) {
+          $response->reason = $connect->connect_error;
+          echo json_encode($response);
+          die;
+      }
       try{
         $sql2="SELECT `FirstName`,`LastName` FROM `tbl_practitioner` WHERE `PractitionerID` = $id";
         $result = mysqli_query($connect, $sql2) or die(mysqli_error($connect));
